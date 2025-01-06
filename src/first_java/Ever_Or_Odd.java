357c77bd0e903a8e97cbd28ce148040d1ba054b5
mayur <mayur@LAGADE>
2025-01-06 09:36:12 +0530

    Program

--- src/first_java/Ever_Or_Odd.java
+++ src/first_java/Ever_Or_Odd.java
@@ -1,7 +1,20 @@
 package first_java;
 
+import java.util.Scanner;
+
 public class Ever_Or_Odd {
 	public static void main(String[] args) {
+		Scanner Sc = new Scanner(System.in);
+		int Number = Sc.nextInt();
+		System.out.println("Ente The number "+Number);
+		
+		if(Number % 2== 0) {
+			System.out.println(Number+" Is a Even Number");
+			
+		}else {
+			System.out.println(Number+" Is a Odd Number");
+		}
+		
 		
 	}
 
